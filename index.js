520c54e68632e61554eb54f1389db65301f7f3c1
Luis Escobar <lescobar.099@gmail.com>
2020-10-15 15:51:56 -0600

    Implementacion de express-handlebars como template engine y ceracion de / con el formulario

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 // Importar Express
 const express = require("express");
 
+// Importar habdlebars como template engine
+const exphbs = require("express-handlebars");
+
 // Crear un servidor express
 const app = express();
 
+// Indicar a express utilizar handlebars como template engine
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs"}));
+
+app.set("view engine", "hbs");
+
 // Crear una ruta
 app.get("/", (req, res, next) => {
-    res. send("Bienvenido!");
+    res. render("formulario_examen");
 });
 
 // Inicializar puerto
